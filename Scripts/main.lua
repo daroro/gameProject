8369f58bb0ff3255377338cf1365249158b2f4de
daroro <58175439+daroro@users.noreply.github.com>
2019-11-27 01:33:31 +0800

    character is able to walk now!

--- Scripts/main.lua
+++ Scripts/main.lua
@@ -1,17 +1,37 @@
 local hero_atlas
 local hero_sprite 
 
+--animation parameters
+local fps = 10
+local anim_timer = 1 / fps
+local frame = 1
+local num_frames = 4
+local xoffset 
+
+
 function love.load()
     love.graphics.setDefaultFilter('nearest','nearest')
     hero_atlas = love.graphics.newImage("Assets/Sprites/characters.png")
-    hero_sprite = love.graphics.newQuad(6,73,18,23,hero_atlas:getDimensions())
+    hero_sprite = love.graphics.newQuad(6,64,32,32,hero_atlas:getDimensions())
 end
 
 function love.update(dt)
-    
+    if dt >0.035 then return end
+
+    anim_timer = anim_timer - dt
+    if anim_timer <= 0 then
+        anim_timer = 1 / fps
+        frame = frame + 1
+
+        if frame > num_frames then
+            frame = 1
+        end
+
+        xoffset = 32 * frame 
+        hero_sprite:setViewport(xoffset, 64, 32, 32)
+    end
 end 
 
 function love.draw()
-    love.graphics.draw(hero_atlas, 25, 25, 0, 1, 1)
-    love.graphics.draw(hero_atlas, hero_sprite, 25, 200, 0, 4, 4)
+    love.graphics.draw(hero_atlas, hero_sprite, 320, 180, 0, 4, 4, 16, 16)
 end
