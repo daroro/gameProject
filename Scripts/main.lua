fccf7442d9ad292126222e24db660d4173579e8e
daroro <58175439+daroro@users.noreply.github.com>
2019-11-27 00:33:09 +0800

    added sprites, reorganised files

--- Scripts/main.lua
+++ Scripts/main.lua
@@ -1,22 +1,17 @@
-local x = 120
-local dir = 1
+local hero_atlas
+local hero_sprite 
 
 function love.load()
-    print("console!")
+    love.graphics.setDefaultFilter('nearest','nearest')
+    hero_atlas = love.graphics.newImage("Assets/Sprites/characters.png")
+    hero_sprite = love.graphics.newQuad(6,73,18,23,hero_atlas:getDimensions())
 end
 
 function love.update(dt)
-    if x >= 500 or x < 100 then
-        dir = dir * -1
-    end
-
-    x = x + dir * 100 * dt
     
 end 
 
 function love.draw()
-    love.graphics.setColor(133,123,30)
-    love.graphics.print("bye", 10, 100)
-    love.graphics.setColor(255,0,0)
-    love.graphics.rectangle("fill",x,100,100,100)
+    love.graphics.draw(hero_atlas, 25, 25, 0, 1, 1)
+    love.graphics.draw(hero_atlas, hero_sprite, 25, 200, 0, 4, 4)
 end
