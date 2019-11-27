b083537bf0be63ec005b73e7037eaef4d226bd72
daroro <58175439+daroro@users.noreply.github.com>
2019-11-28 01:03:59 +0800

    more animations

--- demoGame/main.lua
+++ demoGame/main.lua
@@ -13,15 +13,25 @@ local xoffset
 -----------------------
 
 local spr 
-local walk = Anim(6, 64, 32, 32, 4, 4, 12)
+local walk = Anim(6, 32, 32, 32, 4, 4, 12)
+local jump = Anim(134, 32, 32, 32, 4, 4, 12)
+local hit = Anim(262, 32, 32, 32, 4, 4, 12)
+local climb = Anim(582, 32, 32, 32, 3, 3, 12)
 
 --function the gets carried out when love.exe is loaded
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
     hero_atlas = love.graphics.newImage("Assets/Sprites/characters.png")
-    spr = Sprite(hero_atlas, 32, 32, 100, 100, 1, 1, 0)
+    spr = Sprite(hero_atlas, 32, 32, 100, 100, 5, 5, 0)
     spr:add_animation("walk", walk)
+    spr:add_animation("jump", jump)
+    spr:add_animation("hit", hit)
+    spr:add_animation("climb", climb)
     spr:animate("walk")
+    spr:animate("jump")
+    spr:animate("hit")
+    spr:animate("climb")
+
 end
 
 --function that gets carried out every x milliseconds.
